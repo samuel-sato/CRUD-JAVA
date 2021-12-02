20e29da265b9d9f427d7dc18d0c898f03258afb0
samuel <samuelsato2009@gmail.com>
2021-12-02 14:41:01 -0300

    interface

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,26 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollpane = loader.load();
+			
+			scrollpane.setFitToHeight(true);
+			scrollpane.setFitToWidth(true);
+			
+			Scene mainScene = new Scene(scrollpane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("CRUD JAVAfx");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
