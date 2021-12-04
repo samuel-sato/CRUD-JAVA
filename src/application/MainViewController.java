e2e9e0d6db285997fefc731e62bdb1d6d8c6ce5a
samuel <samuelsato2009@gmail.com>
2021-12-03 22:58:15 -0300

    exibir informações

---
+++ src/application/MainViewController.java
@@ -0,0 +1,93 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.DepartmentListController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
+
+public class MainViewController implements Initializable{
+	@FXML
+	private MenuItem menuItemSeller;
+	@FXML
+	private MenuItem menuItemDepartment;
+	@FXML
+	private MenuItem menuItemAbout;
+	
+	@FXML
+	public void onMenuItemSellerAction() {
+		System.out.println("onMenuItemSellerAction");
+	}
+	
+	@FXML
+	public void onMenuItemDepartmentAction() {
+		loadView2("/gui/DepartmentList.fxml");
+	}
+	
+	@FXML
+	public void onMenuItemAboutAction() {
+		loadView("/gui/About.fxml");
+	}
+
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+	}
+	
+	private synchronized void loadView(String asboluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(asboluteName));
+			VBox newVBox = loader.load();
+			
+			//Atualizando a pagina
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+			Node mainMenu = mainVBox.getChildren().get(0);
+			//limpando a tela
+			mainVBox.getChildren().clear();
+			//adicionando novamente 
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IOException", "Erro loading view", e.getMessage(), AlertType.ERROR);
+			e.printStackTrace();
+		}
+	}
+
+	
+	private synchronized void loadView2(String asboluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(asboluteName));
+			VBox newVBox = loader.load();
+			
+			//Atualizando a pagina
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+			Node mainMenu = mainVBox.getChildren().get(0);
+			//limpando a tela
+			mainVBox.getChildren().clear();
+			//adicionando novamente 
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IOException", "Erro loading view", e.getMessage(), AlertType.ERROR);
+			e.printStackTrace();
+		}
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,13 @@
 module crudJavafx {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
 	
+	//exports javafx.fxml to gui;
+	//javafx.exports;
+	exports gui to javafx.fxml;
+	opens gui to javafx.fxml;
 	opens application to javafx.graphics, javafx.fxml;
+	//exports model.entities to javafx.fxml;
+	opens model.entities to javafx.fxml, javafx.base;
 }
